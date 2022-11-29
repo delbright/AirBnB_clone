5f0d4caa85b192ef20c44207ca8de7e0fcfc67b9
Otswag <106761383+Otswag@users.noreply.github.com>
2022-11-29 23:05:12 +0100

    Updated

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-<<<<<<< HEAD
 """ testing city """
 import unittest
 import pep8
@@ -15,26 +14,3 @@ class City_testing(unittest.TestCase):
         result = pepstylecode.check_files([path_user])
         self.assertEqual(result.total_errors, 0,
                          "Found code style errors (and warnings).")
-=======
-"""Test suite for the City class of the models.city module"""
-import unittest
-
-from models.base_model import BaseModel
-from models.city import City
-
-
-class TestCity(unittest.TestCase):
-    """Test cases for the City class"""
-
-    def setUp(self):
-        self.city = City()
-        self.attr_list = ["state_id", "name"]
-
-    def test_city_is_a_subclass_of_basemodel(self):
-        self.assertTrue(issubclass(type(self.city), BaseModel))
-
-    def test_attrs_are_class_attrs(self):
-        for attr in self.attr_list:
-            self.assertIs(type(getattr(self.city, attr)), str)
-            self.assertFalse(bool(getattr(self.city, attr)))
->>>>>>> d6933edf87fbc1ac580927098c80fed2b66d0899
